1ceb0ff96c270699c063bbba9a281f1bf291062f
msafeem-devops <54424385+msafeem-devops@users.noreply.github.com>
2019-11-12 10:14:31 +1100

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,10 +14,10 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', '');
+define('WP_REDIS_HOST', 'wordpressrd-001.ken9bz.0001.apse2.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','http://wordpressLB-718664736.ap-southeast-2.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpressLB-718664736.ap-southeast-2.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
@@ -30,7 +30,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', '');
+define('DB_HOST', 'wordpress.comkrnclsdob.ap-southeast-2.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
